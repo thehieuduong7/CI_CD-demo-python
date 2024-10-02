dbb337de1db423781e98ce3bdec0b657b3597775
Aung Kyaw Soe <64706759+Agkyaws@users.noreply.github.com>
2024-10-02 22:56:37 +0700

    Create create_table.sql
    adding sql file

---
+++ k8s/create_table.sql
@@ -0,0 +1,8 @@
+USE userdb;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
